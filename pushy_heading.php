15f256ea4d7ee49efdda0a51cbd04f0db841a75e
Michael <webmaster@southernanime.com>
2015-07-07 11:26:40 -0400

    Initial commit

---
+++ pushy_heading.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$title		= $item->anchor_title ? 'title="' . $item->anchor_title . '" ' : '';
+$chevron	= $item->deeper ? ' <i class="right fa fa-chevron-right"></i>' : '';
+
+if ($item->menu_image)
+{
+	$item->params->get('menu_text', 1) ?
+	$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" /><span class="image-title">' . $item->title . '</span> ' :
+	$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" />';
+}
+else
+{
+	$linktype = $item->title;
+}
+?>
+<span class="nav-header <?php echo $item->anchor_css; ?>" <?php echo $title; ?>><?php echo $linktype . $chevron; ?></span>
